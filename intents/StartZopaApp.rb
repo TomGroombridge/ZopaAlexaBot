b2aa0095f0837a1a851358258fe47ee952ada9dd
Tom Groombridge <groombridgetom@gmail.com>
2017-09-08 11:49:43 +0100

    added new messages

--- intents/StartZopaApp.rb
+++ intents/StartZopaApp.rb
@@ -4,10 +4,8 @@ end
 
 intent "Passcode" do
   passcode = request.slot_value("passcode_pin")
-
-  p "this is my passcode #{passcode}"
   if passcode == "1844"
-    respond("thank you, you have now been logged in")
+    respond("thank you, you have now been logged in, how can i help you today?")
   else
     tell("sorry that is not correct")
   end
@@ -35,13 +33,12 @@ intent "AmountOwed" do
 end
 
 intent "AmountSpent" do
-  ask("This week you reached all your spending goals. Your outgoings for this week were £228.34. This is just over £50 less than your typical weekly spend. A payment of this amount towards your Zopa credit card would save you £12.50 in interest if made within the next 30 days. Would you like to hear your credit card balance")
+  respond("This week you reached all your spending goals. Your outgoings for this week were £228.34. This is just over £50 less than your typical weekly spend. A payment of this amount towards your Zopa credit card would save you £12.50 in interest if made within the next 30 days.")
 end
 
 intent "CreditCardBalance" do
-  hear_credit_card = request.slot_value("CreditCardYesNo")
+  hear_credit_card = request.slot_value("see_credit_card_balance")
 
-  puts "hear credit card #{hear_credit_card}"
 
   case hear_credit_card
   when 'yes'
@@ -52,38 +49,24 @@ intent "CreditCardBalance" do
 end
 
 intent "ImproveFinances" do
-  tell("Your current Zopa score is 38. To improve your score you could limit your credit card spending to maximum of £50 per week. Your current average weekly credit card spending is approximately £75.")
+  respond("Your current Zopa score is 38. To improve your score you could limit your credit card spending to maximum of £50 per week. Your current average weekly credit card spending is approximately £75.")
 end
 
 intent "MoreMoney" do
-  tell("Yes Bob, with Zopa you can borrow up to a maximum of £25,000. This can be done
+  respond("Yes Bob, with Zopa you can borrow up to a maximum of £25,000. This can be done
   either by consolidating your current loan whilst applying for additional funds. Or by applying
   for separate loan, as at Zopa you can have more than one loan running at the same time.")
 end
 
 intent "SessionEndedRequest" do
-  tell("ok, thank you for chatting, any queries please let me know")
+  respond("ok, thank you for chatting, any queries please let me know")
 end
 
 intent "CurrentLoan" do
-  tell("To settle your current loan within the next 7 days you would have to pay £2,398.52.")
+  respond("To settle your current loan within the next 7 days you would have to pay £2,398.52.")
 end
 
-intent "SpeakToZoe" do
-  tell("Hi, what is your passcode?")
-end
-
-intent "PasscodeVerified" do
-  tell("Welcome Bob! How can I help you today?")
-end
-
-intent "SpendingTrends" do
-  tell("This week you reached all your spending goals. Your outgoings for this week were £228.34. This is just over £50 less than your typical
-  weekly spend. A payment of this amount towards your Zopa credit card would save you
-  £12.50 in interest if made within the next 30 days. Would you like to hear your credit card
-  balance?")
-end
 
 intent "AprRequest" do
-  tell("APR stands for annual percentage rate and is the interest rate charged for the whole year, rather than month. For example, a loan of £1000 with an APR of 5% would result in a required annual payment of £50. The current APR on your loan is 4.9%, would you like to know more?")
+  respond("APR stands for annual percentage rate and is the interest rate charged for the whole year, rather than month. For example, a loan of £1000 with an APR of 5% would result in a required annual payment of £50. The current APR on your loan is 4.9%.")
 end
