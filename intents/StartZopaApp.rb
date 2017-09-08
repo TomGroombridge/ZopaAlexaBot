410ead5642347b55ec09adc2cef0ea228f7b2f60
Tom Groombridge <groombridgetom@gmail.com>
2017-09-08 11:15:07 +0100

    added new commands

--- intents/StartZopaApp.rb
+++ intents/StartZopaApp.rb
@@ -1,5 +1,16 @@
 intent "LaunchRequest" do
-  respond("You are now in your Zopa application")
+  ask("Hi, this is Zoe, can you please give me your passcode so we can authenticate you")
+end
+
+intent "Passcode" do
+  passcode = request.slot_value("passcode_pin")
+
+  p "this is my passcode #{passcode}"
+  if passcode == "1844"
+    respond("thank you, you have now been logged in")
+  else
+    tell("sorry that is not correct")
+  end
 end
 
 
@@ -8,38 +19,48 @@ intent "CheckBalance" do
 end
 
 intent "SelectAccountType" do
-  p "Select Account Type"
   account = request.slot_value("account")
-  p "account #{account.inspect}"
 
   case account.downcase
   when "isa"
-    tell("OK your zopa balance for your ISA account is £1300")
+    respond("OK your zopa balance for your ISA account is £1300")
   when "standard"
-    tell("OK your zopa balance for your Standard account is £900")
+    respond("OK your zopa balance for your Standard account is £900")
   end
 
 end
 
 intent "AmountOwed" do
-  tell("This month you currently owe £149.99")
+  respond("This month you currently owe £149.99")
 end
 
 intent "AmountSpent" do
-  tell("You have spent £129.69 since the beginning of this week")
+  ask("This week you reached all your spending goals. Your outgoings for this week were £228.34. This is just over £50 less than your typical weekly spend. A payment of this amount towards your Zopa credit card would save you £12.50 in interest if made within the next 30 days. Would you like to hear your credit card balance")
+end
+
+intent "CreditCardBalance" do
+  hear_credit_card = request.slot_value("CreditCardYesNo")
+
+  puts "hear credit card #{hear_credit_card}"
+
+  case hear_credit_card
+  when 'yes'
+    tell("Your credit card balance is £1895")
+  when 'no'
+    tell("ok")
+  end
 end
 
 intent "ImproveFinances" do
-  tell("Your current Zopa score is 38. To improve your score you could limit your credit card
-  spending to maximum of £50 per week. Your current average weekly credit card spending is
-  approximately £75.")
+  tell("Your current Zopa score is 38. To improve your score you could limit your credit card spending to maximum of £50 per week. Your current average weekly credit card spending is approximately £75.")
 end
 
 intent "MoreMoney" do
   tell("Yes Bob, with Zopa you can borrow up to a maximum of £25,000. This can be done
   either by consolidating your current loan whilst applying for additional funds. Or by applying
-  for separate loan as at Zopa you can have more than one loan running at the same time.")
+  for separate loan, as at Zopa you can have more than one loan running at the same time.")
 end
 
-
-
+intent "SessionEndedRequest" do
+  tell("ok, thank you for chatting, any queries please let me know")
+end
--- server.rb
+++ server.rb
@@ -3,34 +3,5 @@ require 'ralyxa'
 
 
 post '/' do
-  p "request: #{request}"
   Ralyxa::Skill.handle(request)
-
-  # query_json = JSON.parse(request.body.read.to_s)
-  # query = AlexaRubykit.build_request(query_json)
-  # p "this is the query #{query}"
-  # p 'this is the request being hit!!!'
-  # p "this is the query type #{query.type}"
-
-  # reply = AlexaRubykit::Response.new
-
-  # if (query.type == 'LAUNCH_REQUEST')
-  #   reply.add_speech('You can now access your Zopa account')
-  # end
-
-  # if (query.type == 'INTENT_REQUEST')
-  #   p "#{query.slots}"
-  #   p "#{query.name}"
-
-  #   case query.name
-  #   when "CheckBalance"
-  #     reply.add_speech("Your Zopa balance is currently £12,580")
-  #   when "SettlementFigure"
-  #     reply.add_speech("Your loan settlemt figure is £8,590")
-  #   else
-  #     reply.add_speech("Sorry we can't give you that information")
-  #   end
-  # end
-
-  # response.body = reply.build_response
 end
